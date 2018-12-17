5d07ff82e4c5489c423ebfd93b9b68616a0ed426
Mohamed Youssef <muhamed-youssef@outlook.com>
2018-12-17 14:22:18 +0200

    added Procfile and static storage

--- stylek/settings.py
+++ stylek/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY') or config['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') or config['DEBUG']
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['stylek.herokuapp.com']
 
 
 # Application definition
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'stylek.urls'
@@ -120,12 +121,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'stylek/static')
 ]
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
