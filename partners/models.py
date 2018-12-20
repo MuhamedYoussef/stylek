0a7c04130f78fa081a5ca4fd432f9ae2f32e23ec
Mohamed Youssef <muhamed-youssef@outlook.com>
2018-12-20 13:52:13 +0200

    Added back button and stars in partner

--- partners/models.py
+++ partners/models.py
@@ -11,17 +11,9 @@ class Partner(models.Model):
     district = models.CharField(max_length=100)
     cats = ArrayField(models.CharField(max_length=100))
     about = models.TextField(blank=True)
+    rating = models.IntegerField(default=3)
     main_photo = models.ImageField(upload_to='partners_profile', default='default.svg')
-    # photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-    # photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-    # photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-    # photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-    # photo_5 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-    # photo_6 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-    # photo_7 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-    # photo_8 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-    # photo_9 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-    # photo_10 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    # list of images
 
     def __str__(self):
         return self.name
