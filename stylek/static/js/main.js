34e9d1831b2ab2fedcfc2d52939b1cfacedaeb23
Mohamed Youssef <muhamed-youssef@outlook.com>
2019-01-22 16:33:37 +0200

    added to top button

--- stylek/static/js/main.js
+++ stylek/static/js/main.js
@@ -2,3 +2,25 @@
 setTimeout(function() {
   $("#message").fadeOut("slow");
 }, 3000);
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    document.getElementById("totop").style.display = "block";
+  } else {
+    document.getElementById("totop").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
